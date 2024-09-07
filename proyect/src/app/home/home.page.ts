e635b612539a52ff34b3c7e7c575b13252261f9b
anto <acampos1307@gmail.com>
2024-09-07 16:41:55 -0400

    Animaciones nuevas + Quienes somos

--- proyect/src/app/home/home.page.ts
+++ proyect/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage{
 
-  constructor() { }
+  private animation!:Animation;
+  constructor(private aCtrl: AnimationController) { }
+
+  isRotating: boolean[] = [false, false, false];
+
+  toggleRotation(index: number) {
+    this.isRotating[index] = !this.isRotating[index];
+  }
+
+  ngAfterViewInit(){
+    this.animation = this.aCtrl.create()
+    .addElement(document.querySelector('.monkey') as HTMLElement)
+    .iterations(1)
+    .duration(2000)
+    .fromTo('transform','translateX(0px) scale(1)','translateX(100px) scale(0.8)')
+    .fromTo('background','red','blue')
+
+    .keyframes([
+      { offset: 0, transform: 'scale(1) rotate(0)' },
+      { offset: 0.4, transform: 'scale(1.5) rotate(180deg)' },
+      { offset: 0.8, transform: 'scale(0.5) rotate(360deg)' },
+      { offset: 1, transform: 'scale(1) rotate(0) ' },
+    ]);
+
+  }
+
+  ejecutar(){
+    this.animation.play()
+  }
+  
 
 
 }
