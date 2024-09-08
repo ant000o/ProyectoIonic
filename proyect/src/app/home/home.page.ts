41c9ef740bdac0331310d0a349e1db72572567b4
anto <acampos1307@gmail.com>
2024-09-08 18:34:59 -0300

    Home mejorado

--- proyect/src/app/home/home.page.ts
+++ proyect/src/app/home/home.page.ts
@@ -6,9 +6,9 @@ import { Animation, AnimationController } from '@ionic/angular';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage{
+export class HomePage {
 
-  private animation!:Animation;
+  private animation!: Animation;
   constructor(private aCtrl: AnimationController) { }
 
   isRotating: boolean[] = [false, false, false];
@@ -17,27 +17,33 @@ export class HomePage{
     this.isRotating[index] = !this.isRotating[index];
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
+    // Configura la animación
     this.animation = this.aCtrl.create()
-    .addElement(document.querySelector('.monkey') as HTMLElement)
-    .iterations(1)
-    .duration(2000)
-    .fromTo('transform','translateX(0px) scale(1)','translateX(100px) scale(0.8)')
-    .fromTo('background','red','blue')
+      .addElement(document.querySelector('.scanner-icon') as HTMLElement)
+      .iterations(1)  // Solo se ejecutará una vez cada vez que se invoque
+      .duration(2000)
+      .fromTo('transform', 'translateX(0px) scale(1)', 'translateX(100px) scale(0.8)')
+      .fromTo('background', 'red', 'blue')
+      .keyframes([
+        { offset: 0, transform: 'scale(1) rotate(0)' },
+        { offset: 0.4, transform: 'scale(1.5) rotate(180deg)' },
+        { offset: 0.8, transform: 'scale(0.5) rotate(360deg)' },
+        { offset: 1, transform: 'scale(1) rotate(0)' },
+      ]);
 
-    .keyframes([
-      { offset: 0, transform: 'scale(1) rotate(0)' },
-      { offset: 0.4, transform: 'scale(1.5) rotate(180deg)' },
-      { offset: 0.8, transform: 'scale(0.5) rotate(360deg)' },
-      { offset: 1, transform: 'scale(1) rotate(0) ' },
-    ]);
+    // Reproduce la animación automáticamente al cargar la vista
+    this.animation.play();
 
+    // Configura que la animación se repita cada 5 segundos (5000 ms)
+    setInterval(() => {
+      this.animation.play();
+    }, 5000); // Cambia el tiempo según la frecuencia deseada
   }
 
-  ejecutar(){
-    this.animation.play()
+  ejecutar() {
+    // Permite reproducir la animación manualmente si se invoca
+    this.animation.play();
   }
-  
-
 
 }
