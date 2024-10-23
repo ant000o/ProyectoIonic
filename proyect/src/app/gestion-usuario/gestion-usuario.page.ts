e8aea635801bb21c366dc69c0d33841bf295b060
Plaza Vespucio <pvespucio@PVAWSB03LC0405.duoc.acad>
2024-10-23 15:35:03 -0300

    Update gestion-usuario.page.ts
    vaciar campos gestion-usuario

--- proyect/src/app/gestion-usuario/gestion-usuario.page.ts
+++ proyect/src/app/gestion-usuario/gestion-usuario.page.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AnimationController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular'; // Importamos AlertController
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-gestion-usuario',
   templateUrl: './gestion-usuario.page.html',
   styleUrls: ['./gestion-usuario.page.scss'],
 })
-
-
 export class GestionUsuarioPage implements OnInit {
 
   // Variables para manejo de usuarios
@@ -18,29 +16,36 @@ export class GestionUsuarioPage implements OnInit {
   lastname: string = '';
   age: number | null = null;
   rol: string = '';
-  // users$: Observable<any[]>;  
-
-
-
   email: string = ''; 
   password: string = ''; 
   showPassword = false;
 
-  constructor(private authService: AuthService, private router: Router, 
-    private animationCtrl: AnimationController, private alertController: AlertController) {}
-  ngOnInit() {
-  }
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private animationCtrl: AnimationController, 
+    private alertController: AlertController
+  ) {}
 
-  
+  ngOnInit() {}
+
   async register() {
     try {
       await this.authService.register(this.email, this.password);
-      this.showAlert('¡Registrado exitosamente!','');
+      this.showAlert('¡Registrado exitosamente!', '');
+
+      // Limpiar los campos después del registro
+      this.email = '';
+      this.password = '';
+      this.name = '';
+      this.lastname = '';
+      this.age = null;
+      this.rol = '';
+      
     } catch (error) {
       this.showAlert('Error', 'Verifique el correo y contraseña.');
     }
   }
-
 
   // Método para mostrar la alerta con ion-alert
   async showAlert(header: string, message: string) {
@@ -52,6 +57,4 @@ export class GestionUsuarioPage implements OnInit {
 
     await alert.present();
   }
-  
-
 }
